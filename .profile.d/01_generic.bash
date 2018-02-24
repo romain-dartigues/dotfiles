c5b871cce12f81554fb4f25f002520ccc8015763
Romain Dartigues <romain.dartigues@gmail.com>
2018-02-24 21:07:48 +0100

    profile: bash: rewrite PS1

--- .profile.d/01_generic.bash
+++ .profile.d/01_generic.bash
@@ -13,6 +13,16 @@ shopt -s histappend
 
 # functions ##################################################################
 
+PS1() {
+	# might require the dangerous: ``sudo -sE``
+	# or, also dangerous: 'Defaults env_keep +='
+	if [ "$UID" = "0" ]
+	then P='\[\033[01;31m\]\u\[\033[0m\]'
+	else P='\u'
+	fi
+	echo "${P}@\h:\W\$ "
+}
+
 
 
 # variables ##################################################################
@@ -37,6 +47,8 @@ export LESS_TERMCAP_so=$'\e[7m'           # stand-out
 export LESS_TERMCAP_us=$'\e[04;38;5;146m' # underline
 export LESS_TERMCAP_me="$LESS_TERMCAP_se" LESS_TERMCAP_ue="$LESS_TERMCAP_se"
 
-export PS1="${PS1/\\u/\$([ \$(id -u) = 0 ]&&printf "\[\033[01;31m\]\u\[\033[0m\]"||printf \u)}"
+export PS1="$(PS1)"
+
+
 
 fi
