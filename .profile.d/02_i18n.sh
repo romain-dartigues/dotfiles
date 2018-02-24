d8ba798720cc76badcc8385b77f5409e7c1820bd
Romain Dartigues <romain.dartigues@gmail.com>
2018-02-24 21:09:19 +0100

    profile: i18n

---
+++ .profile.d/02_i18n.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export TZ='/usr/share/zoneinfo/Europe/Paris'
+
+# values must appear in: ``locale -a``
+export TREE_CHARSET='utf8'
+export LANG='en_US.utf8'
+export LC_TIME='en_DK.utf8'
+
+
