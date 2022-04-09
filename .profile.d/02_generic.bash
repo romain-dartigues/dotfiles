6f9e47b8ffa5900b552695ab31cc85061308ea8c
Romain Dartigues <romain.dartigues@gmail.com>
2022-04-09 20:54:27 +0200

    tempfix PS1

--- .profile.d/02_generic.bash
+++ .profile.d/02_generic.bash
@@ -14,6 +14,4 @@ export LESS_TERMCAP_so=$'\e[7m'           # stand-out
 export LESS_TERMCAP_us=$'\e[04;38;5;146m' # underline
 export LESS_TERMCAP_me="$LESS_TERMCAP_se" LESS_TERMCAP_ue="$LESS_TERMCAP_se"
 
-export PS1="${PS1/\\u/\$([ \$(id -u) = 0 ]&&printf "\[\033[01;31m\]\u\[\033[0m\]"||printf \u)}"
-
 fi
